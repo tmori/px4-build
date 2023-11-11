fbb5e39e2dd7fb605436e0653ff29836a56ef480
Takashi Mori <kanetugu2015@gmail.com>
2023-11-11 12:45:53 +0900

    bug fixed

--- hakoniwa-apps/px4_hakoniwa/control/drone_control.cpp
+++ hakoniwa-apps/px4_hakoniwa/control/drone_control.cpp
@@ -2,7 +2,7 @@
 #include <cstring>
 
 // PIDゲインの定数を定義
-constexpr float DRONE_CONTROL_PID_POS_Z_KP = 2.0f;  // 比例ゲイン
+constexpr float DRONE_CONTROL_PID_POS_Z_KP = 3.0f;  // 比例ゲイン
 constexpr float DRONE_CONTROL_PID_POS_Z_KI = 1.0f;  // 積分ゲイン
 constexpr float DRONE_CONTROL_PID_POS_Z_KD = 0.50f;  // 微分ゲイン
 
@@ -38,7 +38,7 @@ void drone_control_init(DroneControlType& ctrl, double delta_t)
 {
     std::memset(&ctrl, 0, sizeof(ctrl));  // 構造体の全メンバを0で初期化
     ctrl.delta_t = delta_t;
-    ctrl.target_pos.target.z = 6.0f;   // 単位：m
+    ctrl.target_pos.target.z = 10.0f;   // 単位：m
 
     // Z軸用PIDコントローラの初期化
     initPID(ctrl.target_pos.pid_z, DRONE_CONTROL_PID_POS_Z_KP, DRONE_CONTROL_PID_POS_Z_KI, DRONE_CONTROL_PID_POS_Z_KD, ctrl.target_pos.target.z);
